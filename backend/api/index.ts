639feaa4e6adc645b74a85eb8236356d90fa6a3e
sppirana <164136058+sppirana@users.noreply.github.com>
2026-01-04 18:25:24 +0530

    Vercel deployment back

--- backend/api/index.ts
+++
@@ -1,8 +0,0 @@
-import { buildApp, connectDB } from '../src/app';
-
-const app = buildApp();
-
-export default async function handler(req: any, res: any) {
-  await connectDB();
-  return app(req, res);
-}
